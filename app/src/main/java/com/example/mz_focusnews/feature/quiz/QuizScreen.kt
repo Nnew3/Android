6d4043671006d0be93d7a084e114bf66144c47c9
romi <tofha1108@naver.com>
2025-04-03 20:04:23 +0900

    feat: 퀴즈/랭킹 페이지 HorizontalPager 적용

--- app/src/main/java/com/example/mz_focusnews/feature/quiz/QuizScreen.kt
+++ app/src/main/java/com/example/mz_focusnews/feature/quiz/QuizScreen.kt
@@ -1,32 +1,68 @@
 package com.example.mz_focusnews.feature.quiz
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mz_focusnews.core.theme.Bg_Blue
 
 @Composable
 fun QuizScreen() {
-    Box(
+
+    val pagerState = rememberPagerState { 2 }
+
+    Surface(
         modifier = Modifier
             .fillMaxSize()
             .background(Bg_Blue)
-    ) {
-        Text(
-            text = "Quiz",
-            style = MaterialTheme.typography.h1,
-            textAlign = TextAlign.Center,
-            color = Color.Black,
-            modifier = Modifier.align(Alignment.Center)
-        )
+            .padding(25.dp),
+
+        ) {
+        HorizontalPager(
+            modifier = Modifier.fillMaxWidth(),
+            state = pagerState,
+            userScrollEnabled = true,
+        ) { page ->
+            when (page) {
+                0 -> QuizIntroScreen()
+                1 -> RankingScreen()
+            }
+        }
+
+        Row(
+            Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.Top
+        ) {
+            repeat(pagerState.pageCount) { iteration ->
+                val color =  if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+
+                Box(
+                    modifier = Modifier
+                        .padding(12.dp)
+                        .clip(CircleShape)
+                        .background(color)
+                        .size(14.dp)
+                )
+            }
+        }
+
     }
 }
 
