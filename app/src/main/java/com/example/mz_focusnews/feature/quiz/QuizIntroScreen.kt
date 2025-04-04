af5b44dfb1a10409a11a961cd7e8a5c4183613ff
romi <tofha1108@naver.com>
2025-04-04 17:54:47 +0900

    feat: 퀴즈 진입 페이지 구현 및 오늘의 뉴스 확인 여부에 따른 UI 변경 적용

--- app/src/main/java/com/example/mz_focusnews/feature/quiz/QuizIntroScreen.kt
+++ app/src/main/java/com/example/mz_focusnews/feature/quiz/QuizIntroScreen.kt
@@ -1,48 +1,239 @@
 package com.example.mz_focusnews.feature.quiz
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.mz_focusnews.R
+import com.example.mz_focusnews.core.components.QuizRuleDialog
 import com.example.mz_focusnews.core.theme.Bg_Blue
+import com.example.mz_focusnews.core.theme.Blue_900
+import com.example.mz_focusnews.core.theme.Gray_400
+import com.example.mz_focusnews.core.theme.Today_Blue
+import com.example.mz_focusnews.core.theme.preFontFamily
 
 @Composable
-fun QuizIntroScreen() {
+fun QuizIntroScreen(navController: NavController) {
+
+    val checkReadTodayNews = remember { false }
+    val gameBtnColor = if (checkReadTodayNews) Blue_900 else Gray_400
+
+    var openDialog by remember { mutableStateOf(false) }
+
     Surface(
         modifier = Modifier
             .fillMaxSize()
             .background(Bg_Blue)
             .padding(25.dp)
     ) {
-        Box(
+        Column(
             modifier = Modifier
-                .fillMaxSize()
-                .background(Bg_Blue)
+                .fillMaxWidth()
+                .background(Bg_Blue),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Text(
-                text = "Quiz",
-                style = MaterialTheme.typography.h1,
-                textAlign = TextAlign.Center,
-                color = Color.Black,
-                modifier = Modifier.align(Alignment.Center)
+                modifier = Modifier.padding(top = 80.dp),
+                text = "오 늘 의 상 식 키 우 기",
+                style = TextStyle(
+                    fontFamily = preFontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 28.sp
+                )
             )
+
+            Image(
+                painter = painterResource(R.drawable.image_quiz_intro),
+                contentDescription = "Quiz Intro image",
+                modifier = Modifier
+                    .size(200.dp)
+                    .padding(top = 40.dp)
+            )
+
+            Row(
+                modifier = Modifier
+                    .padding(top = 14.dp)
+                    .clickable { openDialog = true },
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Icon(
+                    modifier = Modifier.size(14.dp),
+                    painter = painterResource(R.drawable.icon_quiz_info),
+                    contentDescription = "Quiz Info Icon"
+                )
+
+                Text(
+                    text = "상식 퀴즈 게임 안내",
+                    style = TextStyle(
+                        fontFamily = preFontFamily,
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 12.sp
+                    )
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 20.dp)
+                    .background(Color.White, RoundedCornerShape(12.dp))
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 30.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "현재 래로미 님의 상식 점수는",
+                        style = TextStyle(
+                            fontFamily = preFontFamily,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp
+                        )
+                    )
+
+                    Text(
+                        modifier = Modifier.padding(top = 5.dp),
+                        text = "2,500 점이에요!",
+                        style = TextStyle(
+                            fontFamily = preFontFamily,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp
+                        )
+                    )
+                }
+            }
+
+
+            TextButton(
+                onClick = { },
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .background(gameBtnColor, RoundedCornerShape(18.dp))
+            ) {
+                Text(
+                    text = "게임 시작하기",
+                    style = TextStyle(
+                        fontFamily = preFontFamily,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp,
+                        color = Color.White
+                    ),
+                    modifier = Modifier.padding(horizontal = 28.dp)
+                )
+            }
+
+
+            if (!checkReadTodayNews) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 10.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        Text(
+                            text = "오늘의 뉴스를 봐야 시작 버튼이 활성화 돼요!",
+                            style = TextStyle(
+                                fontFamily = preFontFamily,
+                                fontWeight = FontWeight.Medium,
+                                fontSize = 12.sp
+                            )
+                        )
+
+                        Icon(
+                            modifier = Modifier.size(18.dp),
+                            painter = painterResource(R.drawable.icon_double_arrow),
+                            contentDescription = "Double Arrow"
+                        )
+
+                        Text(
+                            text = "오늘의 뉴스 보러가기",
+                            style = TextStyle(
+                                fontFamily = preFontFamily,
+                                fontWeight = FontWeight.SemiBold,
+                                fontSize = 14.sp
+                            )
+                        )
+
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(Today_Blue, RoundedCornerShape(12.dp))
+                                .clickable { navController.navigate("content") },
+                            contentAlignment = Alignment.CenterStart
+                        ) {
+                            Row(
+                                modifier = Modifier.padding(vertical = 15.dp, horizontal = 15.dp),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Image(
+                                    painter = painterResource(R.drawable.example2),
+                                    contentDescription = "Quiz Intro image",
+                                    modifier = Modifier.width(70.dp)
+                                )
+
+                                Text(
+                                    modifier = Modifier.padding(start = 8.dp),
+                                    text = "\'인도에 진심\' 크래프톤, 현지 게임사 인수한다",
+                                    style = TextStyle(
+                                        fontFamily = preFontFamily,
+                                        fontWeight = FontWeight.SemiBold,
+                                        fontSize = 13.sp
+                                    )
+                                )
+                            }
+                        }
+                    }
+                }
+            }
         }
+    }
+
+    if (openDialog) {
+        QuizRuleDialog(
+            onDismiss = { openDialog = false }
+        )
     }
 }
 
 @Preview
 @Composable
 fun QuizIntroPreview() {
-    QuizIntroScreen()
+    QuizIntroScreen(rememberNavController())
 }
