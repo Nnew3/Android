8c150b5ada7967d3584d03b96c8f20a31605c175
romi <tofha1108@naver.com>
2025-04-21 23:39:31 +0900

    feat: 랭킹 화면 UI 및 Podium 구성 완료

---
+++ app/src/main/java/com/example/mz_focusnews/feature/quiz/RankingListSection.kt
@@ -0,0 +1,124 @@
+package com.example.mz_focusnews.feature.quiz
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mz_focusnews.R
+import com.example.mz_focusnews.core.theme.Bg_Blue
+import com.example.mz_focusnews.core.theme.Gray_200
+import com.example.mz_focusnews.core.theme.Gray_500
+import com.example.mz_focusnews.core.theme.preFontFamily
+
+@Composable
+fun RankingListSection(userList: List<String>) {
+
+    LazyColumn(
+        modifier = Modifier
+            .background(Bg_Blue)
+            .padding(top = 20.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        itemsIndexed(userList) { index, user ->
+            val rank = index + 4 // Podium 1~3등 이후니까 4부터 시작
+            RankingListItem(rank, user)
+        }
+    }
+}
+
+@Composable
+fun RankingListItem(rank: Int, user: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(12.dp))
+            .background(Color.White)
+            .height(100.dp)
+            .padding(12.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxHeight(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(RoundedCornerShape(50))
+                    .border(2.5.dp, Gray_200, RoundedCornerShape(50)),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = rank.toString(),
+                    style = TextStyle(
+                        fontFamily = preFontFamily,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                        color = Color.Gray
+                    )
+                )
+            }
+
+            Image(
+                painter = painterResource(R.drawable.img_person4),
+                contentDescription = "exmaple image",
+                modifier = Modifier
+                    .size(55.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            Column {
+                Text(
+                    text = user,
+                    style = TextStyle(
+                        fontFamily = preFontFamily,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp
+                    )
+                )
+                Text(
+                    text = "점수",
+                    style = TextStyle(
+                        fontFamily = preFontFamily,
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 16.sp,
+                        color = Gray_500
+                    ),
+                    modifier = Modifier.padding(top = 4.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun RankingListPreview() {
+    RankingListSection(listOf("다지니", "래로미", "고양이", "강아지"))
+}
--- app/src/main/java/com/example/mz_focusnews/feature/quiz/RankingScreen.kt
+++ app/src/main/java/com/example/mz_focusnews/feature/quiz/RankingScreen.kt
@@ -1,45 +1,39 @@
 package com.example.mz_focusnews.feature.quiz
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material3.Surface
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mz_focusnews.core.theme.Bg_Blue
 
 @Composable
 fun RankingScreen() {
-    androidx.compose.material.Surface(
+    val userList = listOf("다지니", "래로미", "고양이", "강아지", "강아지", "강아지", "강아지") // 예시용
+
+    Surface(
         modifier = Modifier
             .fillMaxSize()
             .background(Bg_Blue)
             .padding(25.dp)
     ) {
-        Box(
+        Column(
             modifier = Modifier
-                .fillMaxSize()
                 .background(Bg_Blue)
+                .padding(top = 65.dp)
         ) {
-            Text(
-                text = "Ranking",
-                style = MaterialTheme.typography.h1,
-                textAlign = TextAlign.Center,
-                color = Color.Black,
-                modifier = Modifier.align(Alignment.Center)
-            )
+            // 상위 1~3등 Podium
+            RankingPodiumSection()
+
+            // 나머지 유저 랭킹
+            RankingListSection(userList)
+
         }
     }
-
 }
 
 @Preview
