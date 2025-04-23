3a3764187b5d8345fa816d05fde6ab91b6cb4605
romi <tofha1108@naver.com>
2025-04-23 20:08:59 +0900

    feat: ContentViewModel 연결 및 API 연동 구현

--- app/src/main/java/com/example/mz_focusnews/feature/content/ContentScreen.kt
+++ app/src/main/java/com/example/mz_focusnews/feature/content/ContentScreen.kt
@@ -1,6 +1,5 @@
 package com.example.mz_focusnews.feature.content
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,6 +13,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -23,16 +24,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.mz_focusnews.R
+import coil.compose.AsyncImage
 import com.example.mz_focusnews.core.components.BackBtn
 import com.example.mz_focusnews.core.components.BookmarkBtn
 import com.example.mz_focusnews.core.components.RoundedCornerBox
@@ -44,11 +45,21 @@ import com.example.mz_focusnews.core.theme.myiconpack.NonBookmark
 import com.example.mz_focusnews.core.theme.preFontFamily
 
 @Composable
-fun ContentScreen(navController: NavController) {
+fun ContentScreen(
+    viewModel: ContentViewModel,
+    newsId: String,
+    navController: NavController
+) {
+    val contentState = viewModel.contentState.collectAsState().value  // StateFlow 관찰
 
     var isBookmarked by remember { mutableStateOf(false) } // 북마크 여부
 
     val iconRes = if (isBookmarked) MyIconPack.Bookmark else MyIconPack.NonBookmark
+
+    LaunchedEffect(newsId) {
+        val id = newsId.toLongOrNull() ?: 0L
+        viewModel.fetchNewsDetail(id) // id가 변경될 때마다 새로운 데이터를 가져오도록 호출
+    }
 
     Surface(
         modifier = Modifier
@@ -88,122 +99,110 @@ fun ContentScreen(navController: NavController) {
                 )
             }
 
-            LazyColumn {
-                item {
-                    Image(
-                        painter = painterResource(R.drawable.example2),
-                        contentDescription = "exmaple image",
-                        modifier = Modifier
-                            .clip(RoundedCornerShape(12.dp))
-                            .height(210.dp),
-                        contentScale = ContentScale.Crop
-                    )
+            contentState.news?.let { detail ->
+                LazyColumn {
+                    item {
 
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 15.dp),
-                        verticalArrangement = Arrangement.spacedBy(15.dp)
-                    ) {
-                        Text(
-                            text = "의대교수 사직서 제출 행렬…정부 \"5월 2천명 증원 마무리\" 쐐기",
-                            style = TextStyle(
-                                fontFamily = preFontFamily,
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 20.sp
-                            )
+                        // 뉴스 썸네일
+                        AsyncImage(
+                            model = detail.imgUrl,
+                            contentDescription = "News Image",
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(12.dp))
+                                .height(210.dp),
+                            contentScale = ContentScale.Crop
                         )
 
-                        RoundedCornerBox(
-                            radius = 18.dp, bgColor = Blue_900
-                        ) {
-                            Text(
-                                modifier = Modifier.fillMaxWidth(),
-                                text = "정부와 의료계 대화가 둔화되면서 의대 교수들이 사직서를 제출하고 있다.",
-                                style = TextStyle(
-                                    fontFamily = preFontFamily,
-                                    fontWeight = FontWeight.SemiBold,
-                                    fontSize = 14.sp
-                                ),
-                                color = Color.White
-                            )
-                        }
-
-                        RoundedCornerBox(
-                            radius = 18.dp, bgColor = Blue_900
-                        ) {
-                            Text(
-                                modifier = Modifier.fillMaxWidth(),
-                                text = "정부와 의료계 대화가 둔화되면서 의대 교수들이 사직서를 제출하고 있다.",
-                                style = TextStyle(
-                                    fontFamily = preFontFamily,
-                                    fontWeight = FontWeight.SemiBold,
-                                    fontSize = 14.sp
-                                ),
-                                color = Color.White
-                            )
-                        }
-
-                        RoundedCornerBox(
-                            radius = 18.dp, bgColor = Blue_900
-                        ) {
-                            Text(
-                                modifier = Modifier.fillMaxWidth(),
-                                text = "정부와 의료계 대화가 둔화되면서 의대 교수들이 사직서를 제출하고 있다.",
-                                style = TextStyle(
-                                    fontFamily = preFontFamily,
-                                    fontWeight = FontWeight.SemiBold,
-                                    fontSize = 14.sp
-                                ),
-                                color = Color.White
-                            )
-                        }
-                    }
-
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 25.dp),
-                        verticalArrangement = Arrangement.spacedBy(10.dp)
-                    ) {
-                        Text(
-                            text = "의대교수 사직서에 대해 더 알고싶다면?",
-                            style = TextStyle(
-                                fontFamily = preFontFamily,
-                                fontWeight = FontWeight.Medium,
-                                fontSize = 16.sp
-                            ),
+                        // 뉴스 내용
+                        Column(
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .padding(start = 10.dp)
-                        )
-
-                        RoundedCornerBox(
-                            radius = 18.dp, bgColor = Color.White
+                                .padding(top = 15.dp),
+                            verticalArrangement = Arrangement.spacedBy(15.dp)
                         ) {
+
+                            // 뉴스 제목
                             Text(
-                                modifier = Modifier.fillMaxWidth(),
-                                text = "정부와 의료계 대화가 둔화되면서 의대 교수들이 사직서를 제출하고 있다.",
+                                text = detail.title,
                                 style = TextStyle(
                                     fontFamily = preFontFamily,
-                                    fontWeight = FontWeight.SemiBold,
-                                    fontSize = 14.sp
+                                    fontWeight = FontWeight.Bold,
+                                    fontSize = 20.sp
                                 )
                             )
-                        }
 
-                        RoundedCornerBox(
-                            radius = 18.dp, bgColor = Color.White
-                        ) {
-                            Text(
-                                modifier = Modifier.fillMaxWidth(),
-                                text = "정부와 의료계 대화가 둔화되면서 의대 교수들이 사직서를 제출하고 있다.",
-                                style = TextStyle(
-                                    fontFamily = preFontFamily,
-                                    fontWeight = FontWeight.SemiBold,
-                                    fontSize = 14.sp
+                            // 3줄 요약
+                            // summary는 총 세 문장으로만 이루어져있다 (온점도 단 세개만 존재)
+                            val sentences = detail.summary.split(".").map { it.trim() }
+                                .filter { it.isNotEmpty() }
+                            Column(
+                                verticalArrangement = Arrangement.spacedBy(15.dp)
+                            ) {
+                                sentences.forEach { sentence ->
+                                    RoundedCornerBox(
+                                        radius = 18.dp,
+                                        bgColor = Blue_900
+                                    ) {
+                                        Text(
+                                            modifier = Modifier.fillMaxWidth(),
+                                            text = sentence,
+                                            style = TextStyle(
+                                                fontFamily = preFontFamily,
+                                                fontWeight = FontWeight.SemiBold,
+                                                fontSize = 14.sp
+                                            ),
+                                            color = Color.White
+                                        )
+                                    }
+                                }
+                            }
+
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(top = 25.dp),
+                                verticalArrangement = Arrangement.spacedBy(10.dp)
+                            ) {
+                                Text(
+                                    text = "의대교수 사직서에 대해 더 알고싶다면?",
+                                    style = TextStyle(
+                                        fontFamily = preFontFamily,
+                                        fontWeight = FontWeight.Medium,
+                                        fontSize = 16.sp
+                                    ),
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(start = 10.dp)
                                 )
-                            )
+
+                                RoundedCornerBox(
+                                    radius = 18.dp, bgColor = Color.White
+                                ) {
+                                    Text(
+                                        modifier = Modifier.fillMaxWidth(),
+                                        text = "정부와 의료계 대화가 둔화되면서 의대 교수들이 사직서를 제출하고 있다.",
+                                        style = TextStyle(
+                                            fontFamily = preFontFamily,
+                                            fontWeight = FontWeight.SemiBold,
+                                            fontSize = 14.sp
+                                        )
+                                    )
+                                }
+
+                                RoundedCornerBox(
+                                    radius = 18.dp, bgColor = Color.White
+                                ) {
+                                    Text(
+                                        modifier = Modifier.fillMaxWidth(),
+                                        text = "정부와 의료계 대화가 둔화되면서 의대 교수들이 사직서를 제출하고 있다.",
+                                        style = TextStyle(
+                                            fontFamily = preFontFamily,
+                                            fontWeight = FontWeight.SemiBold,
+                                            fontSize = 14.sp
+                                        )
+                                    )
+                                }
+                            }
                         }
                     }
                 }
@@ -215,5 +214,5 @@ fun ContentScreen(navController: NavController) {
 @Preview
 @Composable
 fun ContentPreview() {
-    ContentScreen(rememberNavController())
+    ContentScreen(viewModel(), "0", rememberNavController())
 }
