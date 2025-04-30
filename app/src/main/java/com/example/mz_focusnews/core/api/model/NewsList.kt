bb0651900822251e7a849b9932b322e8a71519b7
romi <tofha1108@naver.com>
2025-05-01 00:12:08 +0900

    feat: 마이페이지 관련 API 모델 및 서비스 구현

---
+++ app/src/main/java/com/example/mz_focusnews/core/api/model/NewsList.kt
@@ -0,0 +1,5 @@
+package com.example.mz_focusnews.core.api.model
+
+data class NewsList(
+    val newsList: List<NewsPreview>
+)
--- app/src/main/java/com/example/mz_focusnews/core/api/model/NewsPreview.kt
+++ app/src/main/java/com/example/mz_focusnews/core/api/model/NewsPreview.kt
@@ -3,4 +3,6 @@ package com.example.mz_focusnews.core.api.model
 data class NewsPreview(
     val id: Long,
     val title: String,
+    val publisher: String = "",
+    val imgUrl: String = ""
 )
