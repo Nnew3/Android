02cbf54957806c06a5d39d0043c062e8760a5dc0
romi <tofha1108@naver.com>
2025-05-01 00:19:23 +0900

    refactor: like, recent screen route에 userId 파라미터 추가

--- app/src/main/java/com/example/mz_focusnews/core/navigation/ScreenRoute.kt
+++ app/src/main/java/com/example/mz_focusnews/core/navigation/ScreenRoute.kt
@@ -2,8 +2,8 @@ package com.example.mz_focusnews.core.navigation
 
 sealed class ScreenRoute(val route: String) {
     object Content : ScreenRoute("content/{newsId}")
-    object Like : ScreenRoute("like")
-    object Recent : ScreenRoute("recent")
+    object Like : ScreenRoute("like/{userId}")
+    object Recent : ScreenRoute("recent/{userId}")
     object QuizIntro : ScreenRoute("quiz_intro")
     object QuizPlay : ScreenRoute("quiz_play")
     object Ranking : ScreenRoute("ranking")
