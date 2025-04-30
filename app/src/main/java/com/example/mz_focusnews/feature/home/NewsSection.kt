1f6b30fbe0fe12b7ee62df19bcabdb42425f12d5
romi <tofha1108@naver.com>
2025-04-30 21:28:06 +0900

    feat: StatusCard 컴포넌트 UI 개선

--- app/src/main/java/com/example/mz_focusnews/feature/home/NewsSection.kt
+++ app/src/main/java/com/example/mz_focusnews/feature/home/NewsSection.kt
@@ -1,16 +1,14 @@
 package com.example.mz_focusnews.feature.home
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mz_focusnews.R
@@ -53,13 +51,11 @@ fun NewsSection(newsState: NewsState, navigateToContent: (id: Long) -> Unit) {
                 else -> ""
             }
 
-            Card(
+            Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .clip(RoundedCornerShape(12.dp))
                     .height(185.dp),
             ) {
-
                 when {
                     // 로딩중
                     newsState.isLoading -> {
